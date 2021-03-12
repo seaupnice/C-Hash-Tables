27ba512b7248764fb2ade32449c35632479a400d
seaupnice <seaupnice@163.com>
2021-03-12 23:24:58 +0800

    Assignments Day1: complete insert, read and delete elements, but not handle collisions.

--- basic_hashtable/b_hashtables.c
+++ basic_hashtable/b_hashtables.c
@@ -71,7 +71,9 @@ unsigned int hash(char *str, int max)
 BasicHashTable *create_hash_table(int capacity)
 {
   BasicHashTable *ht;
-
+  ht = malloc(sizeof(BasicHashTable));
+  ht->capacity = capacity;
+  ht->storage = calloc(capacity, sizeof(Pair*));
   return ht;
 }
 
@@ -84,7 +86,17 @@ BasicHashTable *create_hash_table(int capacity)
  ****/
 void hash_table_insert(BasicHashTable *ht, char *key, char *value)
 {
-
+  unsigned int index = hash(key, ht->capacity);
+  Pair* pair = *(ht->storage + index);
+  if (pair == NULL) {
+    ht->storage[index] = create_pair(key, value);
+    return;
+  }
+  if (strcmp(key, pair->key) != 0) {
+    printf("warning\n");
+  }
+  destroy_pair(pair);
+  ht->storage[index] = create_pair(key, value);
 }
 
 /****
@@ -94,7 +106,10 @@ void hash_table_insert(BasicHashTable *ht, char *key, char *value)
  ****/
 void hash_table_remove(BasicHashTable *ht, char *key)
 {
-
+  unsigned int index = hash(key, ht->capacity);
+  Pair* pair = *(ht->storage + index);
+  destroy_pair(pair);
+  ht->storage[index] = NULL;
 }
 
 /****
@@ -104,6 +119,11 @@ void hash_table_remove(BasicHashTable *ht, char *key)
  ****/
 char *hash_table_retrieve(BasicHashTable *ht, char *key)
 {
+  unsigned int index = hash(key, ht->capacity);
+  Pair* pair = *(ht->storage + index);
+  if (pair != NULL && strcmp(key, pair->key) == 0) {
+    return pair->value;
+  }
   return NULL;
 }
 
@@ -114,7 +134,11 @@ char *hash_table_retrieve(BasicHashTable *ht, char *key)
  ****/
 void destroy_hash_table(BasicHashTable *ht)
 {
-
+  for (int i = 0; i < ht->capacity; i++) {
+    destroy_pair(ht->storage[i]);
+  }
+  free(ht->storage);
+  free(ht);
 }
 
 
